19c5ce756807b503d977c811fd70fad0b0df5f6a
Aislu Kharisova <arslanova1996ais@list.ru>
2024-03-05 16:36:41 +0300

    Дипломная работа. Проект Movies. Бэкенд ветка level-1. Поправки.

--- utils/constants.js
+++ utils/constants.js
@@ -1,4 +1,6 @@
 const allowedCors = [
+  'http://movies.kharisova.nomoredomainsmonster.ru/',
+  'https://movies.kharisova.nomoredomainsmonster.ru/',
   'http://localhost:3000',
   'https://localhost:3000',
   'http://localhost:3001',
